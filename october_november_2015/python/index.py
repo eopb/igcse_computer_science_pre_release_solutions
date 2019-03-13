7319dc1b3a62beacd758a87979144bbd2be8f3a9
Ethan Brierley <ethanboxx@gmail.com>
2019-03-13 17:36:07 +0000

    more code

--- october_november_2015/python/index.py
+++ october_november_2015/python/index.py
@@ -22,4 +22,51 @@ def monitor_baby_regular():
         time.sleep(60)
 
 
-monitor_baby_regular()
+def monitor_baby_regular_array():
+    three_hours_from_now = time.time() + (3 * 60 * 60)
+    # three_hours_from_now = time.time() + 20
+    temperatures = []
+    while time.time() < three_hours_from_now:
+        temperature = input("What is the babies temperature: ")
+        try:
+            temperature = float(temperature)
+        except:
+            print("Please only input numbers")
+            continue
+        temperatures.append(temperature)
+    highest_temp = max(temperatures)
+    lowest_temp = min(temperatures)
+    temp_range = highest_temp - lowest_temp
+    print("The highest temperature was " + str(highest_temp))
+    print("The lowest temperature was " + str(lowest_temp))
+    print("The temperature range was " + str(temp_range))
+
+def monitor_baby_regular_array_warn():
+    # three_hours_from_now = time.time() + (3 * 60 * 60)
+    three_hours_from_now = time.time() + 20
+    temperatures = []
+    while time.time() < three_hours_from_now:
+        temperature = input("What is the babies temperature: ")
+        try:
+            temperature = float(temperature)
+        except:
+            print("Please only input numbers")
+            continue
+        temperatures.append(temperature)
+    highest_temp = max(temperatures)
+    lowest_temp = min(temperatures)
+    temp_range = highest_temp - lowest_temp
+    print("The highest temperature was " + str(highest_temp))
+    print("The lowest temperature was " + str(lowest_temp))
+    print("The temperature range was " + str(temp_range))
+    if temp_range > 1:
+        print("Temperature changed by more than 1 degree.")
+    times_out_of_range = 0
+    for temperature in temperatures:
+        if (temperature <= MIN_SAFE_TEMP) or (temperature > MAX_SAFE_TEMP):
+            times_out_of_range += 1
+    if times_out_of_range > 2:
+        print("Temperature went out of range more than two times.")
+
+
+monitor_baby_regular_array()
